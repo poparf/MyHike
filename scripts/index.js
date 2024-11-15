2b49151fc8062bef9cec2e0cef5078d91cf156f5
Romeo <popescuromeo22@stud.ase.ro>
2024-11-15 16:54:29 +0200

    Added the red and blue markers.

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     let switchPoint = false;
 
     const redIcon = L.icon({
-        iconUrl: 'https://fontawesome.com/icons/location-pin?f=classic&s=solid&pc=%23ff3d3d&sc=%23ff3d3d', // Red marker icon
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',   // Marker shadow
-        iconSize: [25, 41],  // Default size
-        iconAnchor: [12, 41], // Anchor point of the icon
+        iconUrl: '/assets/red-pin.png', // Red marker icon
+        //shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',   // Marker shadow
+        iconSize: [47, 47],  // Default size
+        iconAnchor: [22, 41], // Anchor point of the icon
         popupAnchor: [1, -34], // Popup position relative to icon
         shadowSize: [41, 41]  // Shadow size
     });
+
+    const blueIcon = L.icon({
+        iconUrl: '/assets/blue-pin.png', // Red marker icon
+        //shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',   // Marker shadow
+        iconSize: [40, 40],  // Default size
+        iconAnchor: [20, 41], // Anchor point of the icon
+        popupAnchor: [1, -34], // Popup position relative to icon
+        shadowSize: [41, 41]  // Shadow size
+    })
 
     let startMarker;
     let endMarker;
@@ -40,7 +49,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             if (endMarker) {
                 map.removeLayer(endMarker);
             }
-            endMarker = L.marker([endPoint.lat, endPoint.lng]).addTo(map);
+            endMarker = L.marker([endPoint.lat, endPoint.lng], {
+                icon: blueIcon
+            }).addTo(map);
 
             console.log('End point: ', endPoint);
         }   
